d2d366d25972b824add37341dad7e2eb6988a6d8
Adediran Ayodeji <godonweb1@gmail.com>
2019-05-22 11:45:04 +0200

    Deployed apps linked to portfolio

--- index.php
+++ index.php
@@ -141,7 +141,7 @@ if($_GET["directory"] == "home"){
 	             	<span><i class="fas fa-asterisk"></i>&nbsp;&nbsp;Title :&nbsp;&nbsp;SpaceBot</span><br><br>
 	                <span><i class="fas fa-asterisk"></i>&nbsp;&nbsp;Desc :&nbsp;&nbsp;Built with pure javascript, this app was made to &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;answer questions regarding Codespace Academy. Going &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;open source soon for student contribution.</span><br><br>
 	                <span><i class="fas fa-asterisk"></i>&nbsp;&nbsp;Author:&nbsp;&nbsp;Ayodeji Adediran</span><br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	                <span><a class="button" href="#">Demo</a></span>
+	                <span><a class="button" href="https://spacebot.netlify.com" target="_blank">Demo</a></span>
 	                <span><a class="button" href="#">Github</a></span><br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 	                <span id="technologies"><i class="fab fa-js"></i></span>
 	             </div>
