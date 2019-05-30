c93c3ec3a11dcdcd83e74f21abb464e9004eea6d
Adediran Ayodeji <godonweb1@gmail.com>
2019-05-30 03:11:20 +0200

    Upating mobile view

--- index.php
+++ index.php
@@ -278,7 +278,7 @@ if($_GET["directory"] == "home"){
 		</div><!--wrapper-->	
 	        <footer>
 				<div class="footer">
-					<p class="copyright"><i class="fab fa-twitter-square"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fab fa-linkedin"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fab fa-github-square"></i></p>&nbsp;&nbsp;&nbsp;<span>&copy; 2019 Adediran Ayodeji</span><form><input type="text" name="name" placeholder="Medium updates...">&nbsp;<button>Subscribe&nbsp;&nbsp;<i class="fab fa-medium"></i></button></form>
+					<p class="copyright"><i class="fab fa-twitter-square"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fab fa-linkedin"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fab fa-github-square"></i></p>&nbsp;&nbsp;&nbsp;<span class="copyrightname">&copy; 2019 Adediran Ayodeji</span><form><input type="text" name="name" placeholder="Medium updates...">&nbsp;<button>Subscribe&nbsp;&nbsp;<i class="fab fa-medium"></i></button></form>
 				</div>
 			</footer>
 		</body>
