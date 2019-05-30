93847c4de00824ff386a8d729f6919eeb8d606a0
Adediran Ayodeji <godonweb1@gmail.com>
2019-05-30 19:24:58 +0200

    Update front

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ var container = document.querySelector('.container');
 
 openIcon.addEventListener('click',(e)=>{
     openMenu.style.display = 'block';
+    openMenu.style.animation = '1s fadeInDown';
     //container.style.maxHeight = '50vh';
 })
 
 closeIcon.addEventListener('click',(e)=>{
-    openMenu.style.display = 'none';
+    openMenu.style.animation = '.5s fadeOutUp';
+    setTimeout(()=>{
+       openMenu.style.display = 'none';       
+    },500);
+
 })
--- index.php
+++ index.php
@@ -85,10 +85,10 @@ if($_GET["directory"] == "home"){
                         <div class="open">
                             <i class="fas fa-times"></i>
                             <ul>
-                                <li>Home</li>
-                                <li>Portfolio</li>
-                                <li>About</li>
-                                <li>Contact</li>
+                                <li>&nbsp;&nbsp;Home&nbsp;&nbsp;</li>
+                                <li>&nbsp;&nbsp;Portfolio&nbsp;&nbsp;</li>
+                                <li>&nbsp;&nbsp;About&nbsp;&nbsp;</li>
+                                <li>&nbsp;&nbsp;Contact&nbsp;&nbsp;</li>
                             </ul>
                         </div>
                         
