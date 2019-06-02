d3447d42e014cd6e7ae11ba679cf57e12234041f
Adediran Ayodeji <godonweb1@gmail.com>
2019-06-02 22:09:05 +0200

    Updated

---
+++ poems.php
@@ -0,0 +1,67 @@
+<?php $pageTitle = 'Poems'; ?>
+<!DOCTYPE html>
+<html lang="en">
+    	<head>
+		<title><?php echo $pageTitle ; ?></title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width , initial-scale=1">
+		<meta name="author" content="Adediran Ayodeji">
+		<meta name="description" content="Ayodeji's Online CV">
+		<meta name="keywords" content="Ayodeji Adediran , Online CV , Codespace challenge ">
+        <script src="lottie.js" type="text/javascript"></script>
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic" rel="stylesheet">
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/dejicv.css">
+	</head>
+    
+    <body>
+        <div class="poemdiv">
+            <span class="poemNumbers">1</span><h3>Lamentations</h3>
+            <div class="imagediv">
+                <img src="image/lamentation.jpeg" alt="Lamentations">
+            </div>
+        </div>
+        
+        
+        <div class="poemdiv">
+            <span class="poemNumbers">2</span><h3>Bring back our girls</h3>
+            <div class="imagediv">
+                <img src="image/bringback.png" alt="Bring back our girls">
+            </div>
+        </div>
+        
+        
+        <div class="poemdiv">
+            <span class="poemNumbers">3</span><h3>Stage</h3>
+            <div class="imagediv">
+                <img src="image/stage.png" alt="Stage">
+            </div>
+        </div>
+        
+        
+        <div class="poemdiv">
+            <span class="poemNumbers">4</span><h3>Future</h3>
+            <div class="imagediv">
+                <img src="image/future.jpeg" alt="Future">
+            </div>
+        </div>
+        
+        
+        <div class="poemdiv">
+            <span class="poemNumbers">5</span><h3>Insanity</h3>
+            <div class="imagediv">
+                <img src="image/insanity.jpeg" alt="Insanity">
+            </div>
+        </div>
+        
+        
+        <div class="poemdiv">
+            <span class="poemNumbers">6</span><h3>Till when shall i wait</h3>
+            <div class="imagediv">
+                <img src="image/till.jpeg" alt="Till when shall i wait">
+            </div>
+        </div>
+    </body>
+</html>
