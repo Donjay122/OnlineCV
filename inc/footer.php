961712806f171b88507f25ae5e2ad260aecfdfc2
Adediran Ayodeji <godonweb1@gmail.com>
2019-06-03 21:36:40 +0200

    update social links

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
 		</div><!--wrapper-->	
 	        <footer>
 				<div class="footer">
-					<p class="copyright"><a href="https://twitter.com/donjayodeji" target="_blank"><i class="fab fa-twitter-square"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.linkedin.com/in/donjayodeji" target="_blank"><i class="fab fa-linkedin"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Donjay122" target="_blank"><i class="fab fa-github-square"></i></a></p>&nbsp;&nbsp;&nbsp;<span class="copyrightname">&copy; 2019 Ayodeji Adediran</span><form><input type="text" name="name" placeholder="Medium updates...">&nbsp;<button>Subscribe&nbsp;&nbsp;<i class="fab fa-medium"></i></button></form>
+					<p class="copyright"><a href="https://twitter.com/donjayodeji" target="_blank" style="color:black;"><i class="fab fa-twitter-square"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.linkedin.com/in/donjayodeji" target="_blank"  style="color:black;"><i class="fab fa-linkedin"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Donjay122" target="_blank"  style="color:black;"><i class="fab fa-github-square"></i></a></p>&nbsp;&nbsp;&nbsp;<span class="copyrightname">&copy; 2019 Ayodeji Adediran</span><form><input type="text" name="name" placeholder="Medium updates...">&nbsp;<button>Subscribe&nbsp;&nbsp;<i class="fab fa-medium"></i></button></form>
 				</div>
 
 			</footer>
