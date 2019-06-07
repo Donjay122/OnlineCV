cff8a12fca04b1f6950bcd1f2f1d0011377e3517
Adediran Ayodeji <godonweb1@gmail.com>
2019-06-07 23:07:46 +0200

    Update typo error

--- inc/about.php
+++ inc/about.php
@@ -9,7 +9,7 @@
                 
                 <p>I spend most of my time Coding out ideas by learning online , trying codes out, sketching , solving problems through design thinking which has been fun and challenging ,playing games and writing poems <a href="poems.php" target="_blank">Read Poem</a>.  </p>
                 <p>Delivering backend technologies inline with smooth user expericence became a part of me, because users get to relate to the creativity which is not only for sight-seeing but can also be useful in daily activities. </p>
-                <p>Tools i work with includes, PHP , NodeJS , HTML , CSS ,Javascript , Sketch ... Learning to a developer mean breathing. I'm alive 😁 </p>
+                <p>Tools i work with includes, PHP , NodeJS , HTML , CSS ,Javascript , Sketch ... Learning, to a developer means breathing. I'm alive 😁 </p>
             </div>
             <style>.wrapper{background-image: linear-gradient(white,white);}
                     .nav-list a{color: black;}
